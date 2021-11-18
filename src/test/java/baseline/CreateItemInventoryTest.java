27070039eb081f2591f5b74e6cad40e2fd17c4d3
reyna <reynaldo.martinez1030@gmail.com>
2021-11-18 00:28:02 -0500

    java code

--- src/test/java/baseline/CreateItemInventoryTest.java
+++ src/test/java/baseline/CreateItemInventoryTest.java
@@ -5,6 +5,7 @@ package baseline;
  *  Copyright 2021 Reynaldo Martinez
  */
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,9 +15,20 @@ class CreateItemInventoryTest {
     @Test
     void startProcess() {
         //create an actual item
+        InventoryItem actual = new InventoryItem();
         //set the values for actual item
+        actual.setName("PS5");
+        actual.setSerialNumber("A-XB1-24A-XY3");
+        actual.setValue(1499.00);
         //create an instance of item
+        InventoryItem testing = new InventoryItem();
         //set the values for instance of item
+        testing.setName("PS5");
+        testing.setSerialNumber("A-XB1-24A-XY3");
+        testing.setValue(1499.00);
         //assertEquals if the creation of the item is correct
+        Assertions.assertEquals(actual.getName(), testing.getName());
+        Assertions.assertEquals(actual.getSerialNumber(), testing.getSerialNumber());
+        Assertions.assertEquals(actual.getValue(), testing.getValue());
     }
 }
--- src/test/java/baseline/InventoryItemTest.java
+++ src/test/java/baseline/InventoryItemTest.java
@@ -5,6 +5,8 @@ package baseline;
  *  Copyright 2021 Reynaldo Martinez
  */
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,24 +16,36 @@ class InventoryItemTest {
     @Test
     void testName(){
         //create an actual string
+        SimpleStringProperty actual = new SimpleStringProperty("PS5");
         //create a new item
+        InventoryItem testing = new InventoryItem();
         //set the name
+        testing.setName("PS5");
         //assertEquals actual with testing.getName
+        assertEquals(actual.get(), testing.getName());
     }
 
     @Test
     void testSerialNumber(){
         //create an actual string
+        SimpleStringProperty actual = new SimpleStringProperty("A-XB1-24A-XY3");
         //create a new item
+        InventoryItem testing = new InventoryItem();
         //set the serialNumber
+        testing.setSerialNumber("A-XB1-24A-XY3");
         //assertEquals actual with testing.getSerialNumber
+        assertEquals(actual.get(), testing.getSerialNumber());
     }
 
     @Test
     void testValue(){
         //create an actual double
+        SimpleDoubleProperty actual = new SimpleDoubleProperty(1499.99);
         //create a new item
+        InventoryItem testing = new InventoryItem();
         //set the value
+        testing.setValue(1499.99);
         //assertEquals actual with testing.getValue
+        assertEquals(actual.get(), testing.getValue());
     }
 }
