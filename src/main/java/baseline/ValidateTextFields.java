814446d352d66b079cd86a23564e10b630c3cbd5
reyna <reynaldo.martinez1030@gmail.com>
2021-11-18 19:07:01 -0500

    java code

--- src/main/java/baseline/ValidateTextFields.java
+++ src/main/java/baseline/ValidateTextFields.java
@@ -7,38 +7,63 @@ package baseline;
 
 public class ValidateTextFields {
 
+    //create string numRegex and alphaRegex
+    String numRegex = ".*[0-9].*";
+    String alphaRegex = ".*[A-Za-z].*";
+
     public boolean validateName(String name){
-        //create a boolean to return
+        //trim the result to avoid empty names
+        String trimmedName = name.trim();
         //create an if statement to check if description is null or if description is longer than 256 or if description is less than 2
+        if(trimmedName.length() > 256 || trimmedName.length() < 2 || trimmedName.isBlank())
             //return false if name is equal to any of these
-            //call popup for invalid data
-        //else return true
-        return false;
+            return true;
+        else{
+            //create instance of popup
+            PopupMessage popup = new PopupMessage();
+            //call the corresponding popUp message for invalid name
+            popup.invalidName();
+            return false;
+        }
     }
 
     public boolean validateSerialNumber(String serialNumber){
-        //create a boolean to return
-
-        //check if the first letter is a number followed by a dash
-            //if not then return false
-            //call popup for invalid data
-        //check if next set of three numbers is either letters or numbers
-            //if not then return false
-            //call popup for invalid data
-        //check if the last set of is numbers or letters
-            //if not then return false
-            //call popup for invalid data
-        //check if the value is inside the observableList already
-            //if it is return false call popup
-        return false;
+        //create an instance of popUp
+        PopupMessage popup = new PopupMessage();
+        //create array for string parts
+        String[] serialNumberParts = serialNumber.split("(-)");
+         //check if the number of parts is equal to 4 if not set actualResult to false
+        if(serialNumberParts.length != 4){
+            //call the corresponding popUp message for invalid name
+            popup.invalidSerialNumber();
+            return false;
+        }else{
+            if(
+                    (serialNumberParts[0].matches(alphaRegex))
+                    && (serialNumberParts[1].matches(numRegex) || serialNumberParts[1].matches(alphaRegex))
+                    && (serialNumberParts[2].matches(numRegex) || serialNumberParts[2].matches(alphaRegex))
+                    && (serialNumberParts[3].matches(numRegex) || serialNumberParts[3].matches(alphaRegex))
+            ){
+                return true;
+            }else{
+                //call the corresponding popup message
+                popup.invalidSerialNumber();
+                return false;
+            }
+        }
     }
 
     public boolean validateValue(double value){
-        //create a boolean to return
-
-        //check if the value is greater than or equal to zero
-            //if not then return false
-            //call the popup for invalid data
-        return false;
+        //check if the value is greater than 0.00
+        if(value >= 0.00)
+            return true;
+        else{
+            //create instance of popup
+            PopupMessage popup = new PopupMessage();
+            //call the corresponding popUp message for invalid name
+            popup.invalidValue();
+            //return false
+            return false;
+        }
     }
 }
--- src/test/java/baseline/ValidateTextFieldsTest.java
+++ src/test/java/baseline/ValidateTextFieldsTest.java
@@ -6,7 +6,6 @@ package baseline;
  */
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ValidateTextFieldsTest {
@@ -14,34 +13,73 @@ class ValidateTextFieldsTest {
     @Test
     void validateName() {
         //create an actual boolean to set values
-        //create an actual string name
+        boolean actualResult;
+        //create a string to test
+        String input = "PS5 Ultimate Edition";
+        //strip the whitespace to determine legal input
+        String trimmedInput = input.trim();
         //create an if statement to check if description is null or if description is longer than 256 or if description is less than 2
+        if(trimmedInput == null || trimmedInput.length() > 256 || trimmedInput.length() < 2 || trimmedInput.isBlank())
             //set actual to false if name is equal to any of these
+            actualResult = false;
+        else
+            actualResult = true;
         //assertTrue/assertFalse for string name
+        assertTrue(actualResult);
     }
 
+    //the serial Number must have 3 dashes if it doesn't set return false
+    //will split serial number into 4 parts
+    //first part must contain a letter if not return false
+    //second part can either be letter or digit and 3 letters only if not return false
+    //third part same thing
+    //fourth part same thing
     @Test
     void validateSerialNumber() {
+        //create string numRegex and alphaRegex
+        String numRegex = ".*[0-9].*";
+        String alphaRegex = ".*[A-Za-z].*";
         //create an actual boolean to set values
+        boolean actualResult;
         //create an actual string serialNumber
-
-        //check if the first letter is a number followed by a dash
-            //if not then set actual to false
-        //check if next set of three numbers is either letters or numbers
-            //if not then set actual to false
-        //check if the last set of is numbers or letters
-            //if not then set actual to false
-        //check if the value is inside the observableList already
-            //if it is then set actual to false
+        String inputSerialNumber = "A-xb1-244-Xy3";
+        //create array for string parts
+        String[] parts = inputSerialNumber.split("(-)");
+        //check if the number of parts is equal to 4 if not set actualResult to false
+        if(parts.length != 4){
+            //call the popUp menu
+            //set actual result to false
+            actualResult = false;
+        }else{
+            //check if each part contains either numbers or digits
+            if(
+                    (parts[0].matches(alphaRegex))
+                    && (parts[1].matches(numRegex) || parts[1].matches(alphaRegex))
+                    && (parts[2].matches(numRegex) || parts[2].matches(alphaRegex))
+                    && (parts[3].matches(numRegex) || parts[3].matches(alphaRegex))
+            ){
+                actualResult = true;
+            }else{
+                //call the corresponding popup message
+               //set the actual result to false
+                actualResult = false;
+            }
+        }
         //assertTrue/assertFalse for string name
+        assertTrue(actualResult);
     }
 
     @Test
     void validateValue() {
         //create an actual boolean to set values
+        boolean actualResult = false;
         //create an actual double value
+        double actualValue = 1499.99;
         //check if the value is greater than or equal to zero
+        if(actualValue >= 0.00)
             //if not then set actual to false
+            actualResult = true;
         //assertTrue/assertFalse for string name
+        assertTrue(actualResult);
     }
 }
