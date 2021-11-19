1e3a0c0ca3e1bbf710a9ca79e41d58d48fb1ab69
reyna <reynaldo.martinez1030@gmail.com>
2021-11-18 19:10:23 -0500

    java code

--- src/main/java/baseline/CreateItemInventory.java
+++ src/main/java/baseline/CreateItemInventory.java
@@ -9,9 +9,19 @@ public class CreateItemInventory {
 
     public InventoryItem startProcess(String name, String serialNumber, double value){
         //create an instance of inventoryItem
+        InventoryItem newItem = new InventoryItem();
         //create an instance of ValidateTextFields
+        ValidateTextFields validate = new ValidateTextFields();
         //check if each parameter either passes or not
-        //if they all pass then return the new item
-        return null;
+        if((validate.validateName(name) && validate.validateSerialNumber(serialNumber) && validate.validateValue(value))){
+            //set the new item variables
+            newItem.setName(name);
+            newItem.setSerialNumber(serialNumber);
+            newItem.setValue(value);
+            //return the new item
+            return newItem;
+        }else{
+            return null;
+        }
     }
 }
