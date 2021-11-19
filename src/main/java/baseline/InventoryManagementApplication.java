0211a8d8ee07864e5e72230a51ea81c8ec61ba9d
reyna <reynaldo.martinez1030@gmail.com>
2021-11-18 19:54:42 -0500

    pseudo code

--- src/main/java/baseline/InventoryManagementApplication.java
+++ src/main/java/baseline/InventoryManagementApplication.java
@@ -1,9 +1,35 @@
 package baseline;
 
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
 /*
  *  UCF COP3330 Fall 2021 Application Assignment 2 Solution
  *  Copyright 2021 Reynaldo Martinez
  */
 
-public class InventoryManagementApplication {
+public class InventoryManagementApplication extends Application{
+    @Override
+    public void start(Stage stage) throws Exception{
+        //create a setOnCloseRequest
+            //create instance of popup
+            //check the returned value of popup
+                //quit program based on value
+                //else consume event
+        //create a parent root
+        //attach scene graph to scene
+        //display in window's title bar
+        //attach scene to stage
+        //display the stage
+    }
+
+    public static void main(String[] args){
+        //create an object and call its start method
+    }
 }
